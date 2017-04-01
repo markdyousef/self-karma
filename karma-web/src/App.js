d8bc77a90b98bd7b59f0ff2791bf71c930ca7270
markdyousef <mark@clai.io>
2017-04-01 21:28:00 +0200

    added eslint to project

--- karma-web/src/App.js
+++ karma-web/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Journal from './components/Journal';
 
 injectTapEventPlugin();
 
@@ -8,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-          <div>Jello</div>
+          <Journal />
       </MuiThemeProvider>
     );
   }
---
+++ karma-web/src/components/Journal/Journal.js
@@ -0,0 +1,15 @@
+import React, { PropTypes, Component } from 'react';
+
+export default class Journal extends Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+    render() {
+        return(
+            <div>
+                JOURNAL
+            </div>
+        );
+    }
+}
