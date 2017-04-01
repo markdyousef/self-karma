d21993c30e747ec8f416489372c74ec9b4f2fc9d
markdyousef <mark@clai.io>
2017-04-01 21:47:21 +0200

    added Paper component to journal

--- karma-web/src/components/Journal/Journal.js
+++ karma-web/src/components/Journal/Journal.js
@@ -1,5 +1,23 @@
 import React, { PropTypes, Component } from 'react';
 import Paper from 'material-ui/Paper/Paper';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #ddd;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const paperStyle = {
+    height: '80%',
+    width: '80%',
+    maxWidth: '400px',
+    textAlign: 'center',
+}
 
 export default class Journal extends Component {
     constructor() {
@@ -8,9 +26,9 @@ export default class Journal extends Component {
     }
     render() {
         return(
-            <Paper>
-                JOURNAL
-            </Paper>
+            <Container>
+                <Paper style={paperStyle} zDepth={1} />
+            </Container>
         );
     }
 }
