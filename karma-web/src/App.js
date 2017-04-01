fe79c1767bc2a59009dd5fffcacda64d633a4b75
markdyousef <mark@clai.io>
2017-04-01 20:42:09 +0200

    added journal component

--- karma-web/src/App.js
+++ karma-web/src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import Paper from 'material-ui/Paper';
+
+const Container = styled.div`
+    padding: 50px;
+    width: 100%;
+    height: 100%;
+
+`;
 
 class App extends Component {
   render() {
     return (
-      <div>
-          NEW
-      </div>
+      <Container>
+          <Paper />
+      </Container>
     );
   }
 }
