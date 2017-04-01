419e70c5ec171cec363737c62e573324a56fd443
markdyousef <mark@clai.io>
2017-04-01 20:33:05 +0200

    added material-ui and styled components to package

--- karma-web/src/index.js
+++ karma-web/src/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+
+injectTapEventPlugin();
+
+const Provider = () => (
+    <MuiThemeProvider>
+            <App />
+    </MuiThemeProvider>
+);
 
 ReactDOM.render(
-  <App />,
+  <Provider />,
   document.getElementById('root')
 );
