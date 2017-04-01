bf3dcca05874fcf045a125a62b6f6de61fb5efae
markdyousef <mark@clai.io>
2017-04-01 20:58:14 +0200

    Revert "added material-ui and styled components to package"
    This reverts commit 419e70c5ec171cec363737c62e573324a56fd443.

--- karma-web/src/App.js
+++ karma-web/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-import Paper from 'material-ui/Paper';
+import logo from './logo.svg';
+import './App.css';
 
 const Container = styled.div`
     padding: 50px;
@@ -12,9 +12,15 @@ const Container = styled.div`
 class App extends Component {
   render() {
     return (
-      <Container>
-          <Paper />
-      </Container>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
--- karma-web/src/index.js
+++ karma-web/src/index.js
@@ -2,19 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-
-
-injectTapEventPlugin();
-
-const Provider = () => (
-    <MuiThemeProvider>
-            <App />
-    </MuiThemeProvider>
-);
 
 ReactDOM.render(
-  <Provider />,
+  <App />,
   document.getElementById('root')
 );
