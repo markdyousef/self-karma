9f1b677470027e10176ea45bbc8756b5dfafbe06
markdyousef <mark@clai.io>
2017-04-05 21:31:46 +0200

    added query for single person

--- karma_server/journal/schema.py
+++ karma_server/journal/schema.py
@@ -10,7 +10,24 @@ class ProfileType(DjangoObjectType):
 
 
 class Query(graphene.AbstractType):
+    profile = graphene.Field(
+        ProfileType,
+        id=graphene.String()
+        # name=graphene.String()
+    )
     all_profiles = graphene.List(ProfileType)
 
     def resolve_all_profiles(self, args, context, info):
         return Profile.objects.all()
+
+    def resolve_profile(self, args, context, info):
+        id = args.get('id')
+        name = args.get('name')
+
+        if id is not None:
+            return Profile.objects.get(pk=id)
+
+        if name is not None:
+            return Profile.objects.get(name=name)
+
+        return None
